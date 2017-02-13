4cc2b03c6919b8b25a4147a52d75bd164cf9c1b8
misha-pelykh <jscript@meta.ua>
2017-02-13 16:43:08 +0200

    added host to prodaction enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_controller.allow_forgery_protection = false
+
+  config.action_mailer.default_url_options = { host: 'dkeeper-api-server.herokuapp.com' }
 end
