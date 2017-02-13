bb7cb6eec2e3404554850aebf4cdbec60996da76
misha-pelykh <jscript@meta.ua>
2017-02-13 21:50:55 +0200

    remove 4 spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
     def confirmation_token
       if self.confirm_token.blank?
-          self.confirm_token = SecureRandom.urlsafe_base64.to_s
+        self.confirm_token = SecureRandom.urlsafe_base64.to_s
       end
     end
 end
